f833d6a8ecfe9e46445f0f26c99ef44d4ac51cf8
Renn33GF <andrejkvartalnyj472@gmail.com>
2025-12-02 13:41:30 +0300

    creating JS code, complited 3 tasks

---
+++ JS1.js
@@ -0,0 +1,13 @@
+/*task 1*/
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+/*task 2*/
+const releaze_iphone_data = 2007;
+alert(releaze_iphone_data);
+
+/*task 3*/
+const author_name = "Брендан Эйх";
+alert(author_name);

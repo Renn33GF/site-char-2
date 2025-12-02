cb17f86749fe3129ab31f4d4b7ec3e80edd55310
Renn33GF <andrejkvartalnyj472@gmail.com>
2025-12-02 13:44:36 +0300

    creating 4 task

--- JS1.js
+++ JS1.js
@@ -11,3 +11,12 @@ alert(releaze_iphone_data);
 /*task 3*/
 const author_name = "Брендан Эйх";
 alert(author_name);
+
+
+/*task4*/
+const first_num = 10;
+const second_num = 2;
+alert(first_num + second_num);
+alert(first_num - second_num);
+alert(first_num * second_num);
+alert(first_num / second_num);
